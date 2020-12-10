4c33f4c5d4cebe8f6e3e2af3e5fd7271ec8d1347
lindahu123 <lhu@olin.edu>
2020-12-10 07:07:55 -0800

    Add function to trim data

---
+++ TrimMotionData.m
@@ -0,0 +1,32 @@
+function [chunked_t_Accel,chunked_Accel] = TrimMotionData(MotionData,time_trim)
+%TRIMMOTIONDATA Summary of this function goes here
+Fs = MotionData.accel_rate % in hertz
+N = length(MotionData.t_Accel)
+
+% FIlter data into 1 minute chunk
+chunk_sz = 60 % time in seconds
+chunks = 10
+chunk_sz_index = round(chunk_sz * Fs)
+front_trim = time_trim % trim the first ten
+
+% Trim the entire front
+index_front = round(front_trim * Fs)+1
+MotionData.t_Accel = MotionData.t_Accel(index_front:end)
+MotionData.t_Accel = MotionData.t_Accel - MotionData.t_Accel(1);
+MotionData.Accel = MotionData.Accel(index_front:end, :);
+
+
+chunked_Accel = {}
+chunked_t_Accel = {}
+for i=1:chunks
+    begin_splice = (i-1)*chunk_sz_index+1;
+    end_splice = (i)*chunk_sz_index;
+    if end_splice > length(MotionData.t_Accel)
+        break
+    end
+    % Chunk the data
+    chunked_t_Accel{end + i} = MotionData.t_Accel(begin_splice:end_splice);
+    chunked_Accel{end + i} = MotionData.Accel(begin_splice:end_splice, :);
+    % Take a chunks worht of data
+end
+end
